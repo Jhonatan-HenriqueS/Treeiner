574a8ea5964c448748f761283db3f8128e42aa7c
Jhonatan-HenriqueS <jhonatanhrcomercial@gmail.com>
2026-02-06 15:41:11 -0400

    feat: add description

--- src/app/componentes/header.tsx
+++ src/app/componentes/header.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import * as React from "react";
+import { Calendar } from "@/components/ui/calendar";
 import {
   AlertDialog,
   AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
@@ -13,33 +14,33 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Plus } from "lucide-react";
+import { Plus, Trash2Icon } from "lucide-react";
 import { useState } from "react";
 
 const Header = () => {
-  const [title, setTitle] = useState<string>("");
-  const [showError, setShowError] = useState<boolean>(false);
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
-  const checkEnable = title.length > 0;
+  const [title, setTitle] = useState<string>(""); //Guarda o título inserido pelo usuário
 
-  const handleClick = () => {
-    if (title.trim().length === 0) {
-      setShowError(true);
-    }
-  };
+  const [description, setDescription] = useState<string>(""); //Guarda a descrição inserida pelo usuário
+
+  const checkEnable = title.length > 0; //Verifica se o título não está vazio para habilitar o botão
+
   return (
     <main>
       <h1 className="text-lg font-medium text-center">Adicione sua tarefa</h1>
       <section className="flex items-center mt-6 gap-3">
         <div>
           <Input
-            className="bg-primary text-white text-center border-none p-6 rounded-3xl
-            placeholder:text-white focus:placeholder:opacity-40 transition-all
+            className="bg-primary disabled:bg-primary text-white text-center border-none p-6 rounded-3xl
+            placeholder:text-gray-500 focus:placeholder:opacity-40 transition-all
             focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary"
             type="text"
             placeholder="Insira o título"
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
           />
         </div>
         <AlertDialog>
@@ -51,28 +52,64 @@ const Header = () => {
               <Plus className="mx-2" />
             </Button>
           </AlertDialogTrigger>
-          <AlertDialogContent className="flex flex-col justify-center items-center ">
+          <AlertDialogContent className="flex flex-col justify-center items-center p-6 ">
             <AlertDialogHeader>
               <AlertDialogTitle className="w-full text-center">
                 Descreva como deverá ser seu check-list
               </AlertDialogTitle>
-              <Input
-                className="mt-5 bg-primary text-white text-center border-none p-6 rounded-3xl
-            placeholder:text-white focus:placeholder:opacity-40 transition-all
+              <section className="flex flex-col gap-5">
+                <div>
+                  <Input
+                    className="bg-primary disabled:bg-primary text-white text-center border-none p-6 rounded-3xl
+            placeholder:text-gray-500 focus:placeholder:opacity-40 transition-all
             focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary"
-                type="text"
-                placeholder={title}
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <AlertDialogDescription className="mt-4">
-                This action cannot be undone. This will permanently delete your
-                account from our servers.
-              </AlertDialogDescription>
+                    type="text"
+                    placeholder={title}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <h2 className="w-full text-center font-medium ">
+                    Descreva como deverá ser esta tarefa:
+                  </h2>
+                  <Input
+                    className="mt-2 bg-primary disabled:bg-primary text-white text-center border-none p-6 rounded-3xl
+            placeholder:text-gray-500 focus:placeholder:opacity-40 transition-all
+            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary"
+                    type="text"
+                    placeholder="Descrição da tarefa"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <h2 className="w-full text-center font-medium mb-2">
+                    Para quando deverá ser finalizado esta tarefa?
+                  </h2>
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-lg border "
+                    captionLayout="dropdown"
+                  />
+                </div>
+              </section>
             </AlertDialogHeader>
-            <div>
-              <h2>{title}</h2>
-            </div>
+            <section className="bg-gray-200 p-4 rounded-4xl my-4 w-full">
+              <div className="flex justify-between">
+                <h2 className="text-center font-medium">{title}</h2>
+                <p className="text-center">xx/xx/xxxx xx:xx</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="ml-4 mt-3 text-gray-700">{description}</p>
+                <Trash2Icon
+                  size={16}
+                  className="mr-4 rounded-full p-5 z-50 text-white"
+                />
+              </div>
+            </section>
             <AlertDialogFooter>
               <AlertDialogCancel>Cancel</AlertDialogCancel>
               <AlertDialogAction>Continue</AlertDialogAction>
